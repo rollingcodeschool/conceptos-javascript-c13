4e76da9f79ee045adcc6930f249410448afd5a7a
Emilse Arias <emilsearias@gmail.com>
2025-06-03 21:36:16 -0300

    agregue el readme del proyecto

---
+++ js/01-variables.js
@@ -0,0 +1,17 @@
+// Comentario de una sola linea
+
+/*
+Esto es un comentario de
+multiples
+lineas
+*/
+
+// crear una variable (let - const - var)
+
+let numero1;
+
+let anio = 2025
+
+//mostrar el valor de las variables
+console.log('Hola mundo')
+console.log(numero1, anio)
