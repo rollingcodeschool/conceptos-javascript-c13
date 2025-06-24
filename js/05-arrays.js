c6cc2abd3dec172baa942d18985f8ad31f090e5d
Emilse Arias <emilsearias@gmail.com>
2025-06-24 20:14:12 -0300

    agregamos una funcion para mostrar el array de canciones

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -1,3 +1,14 @@
+const mostrarCanciones = (titulo) => {
+  document.writeln(
+    `<h2 class='mt-3'>${titulo} - cantidad de elementos ${canciones.length}</h2>`
+  );
+  document.writeln("<ul class='list-group'>");
+  for (let i = 0; i < canciones.length; i++) {
+    document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
+  }
+  document.writeln("</ul>");
+};
+
 // como declarar un array
 // array vacio
 const peliculas = [];
@@ -18,80 +29,30 @@ document.writeln(
   `<p>Mostrar el elemento de la posicion 30 del array: ${canciones[30]}</p>`
 );
 
-document.writeln("<h2 class='mt-3'>Mostrar el array de canciones</h2>");
-document.writeln("<ul class='list-group'>");
-for (let i = 0; i < canciones.length; i++) {
-  document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
-}
-document.writeln("</ul>");
-
+mostrarCanciones("Mostrar el array de canciones");
 // agregar elementos al array
 
-document.writeln("<h2 class='mt-3'>Agregar un elemento al inicio del array</h2>");
-canciones.unshift(false, 'like stone' )
+canciones.unshift(false, "like stone");
+mostrarCanciones("Agregar un elemento al inicio del array");
 
-document.writeln("<ul class='list-group'>");
-for (let i = 0; i < canciones.length; i++) {
-  document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
-}
-document.writeln("</ul>");
+canciones.push("like a Rolling stone");
+mostrarCanciones("Agregar un elemento al final del array");
 
-document.writeln("<h2 class='mt-3'>Agregar un elemento al final del array</h2>");
-canciones.push('like a Rolling stone')
-
-document.writeln("<ul class='list-group'>");
-for (let i = 0; i < canciones.length; i++) {
-  document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
-}
-document.writeln("</ul>");
-
-canciones.splice(4,0,'feel Good inc')
-document.writeln(`<h2 class='mt-3'>Agregar un elemento en el medio del array - cantidad de elementos ${canciones.length}</h2>`);
-
-document.writeln("<ul class='list-group'>");
-for (let i = 0; i < canciones.length; i++) {
-  document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
-}
-document.writeln("</ul>");
+canciones.splice(4, 0, "feel Good inc");
+mostrarCanciones("Agregar un elemento en el medio del array");
 
 //borrar elementos del array
 canciones.shift();
+mostrarCanciones("Borrar un elemento del inicio del array");
 
-document.writeln(`<h2 class='mt-3'>Borrar un elemento del inicio del array - cantidad de elementos ${canciones.length}</h2>`);
-
-document.writeln("<ul class='list-group'>");
-for (let i = 0; i < canciones.length; i++) {
-  document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
-}
-document.writeln("</ul>");
-
-canciones.pop()
-document.writeln(`<h2 class='mt-3'>Borrar un elemento del final del array - cantidad de elementos ${canciones.length}</h2>`);
-
-document.writeln("<ul class='list-group'>");
-for (let i = 0; i < canciones.length; i++) {
-  document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
-}
-document.writeln("</ul>");
-
- canciones.splice(2,1)
+canciones.pop();
+mostrarCanciones("Borrar un elemento del final del array");
+canciones.splice(2, 1);
 // canciones.splice(2,3) //desde la posicion 2 del array borro 3 elementos
 // canciones.splice(2) //borrar todo desde la posicion 2 del array
 // canciones.splice( canciones.length - 1 ,1)
-document.writeln(`<h2 class='mt-3'>Borrar un elemento del medio del array - cantidad de elementos ${canciones.length}</h2>`);
-
-document.writeln("<ul class='list-group'>");
-for (let i = 0; i < canciones.length; i++) {
-  document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
-}
-document.writeln("</ul>");
+mostrarCanciones("Borrar un elemento del medio del array");
 
 //modificar elementos del array
-canciones[3] = 'Sobredosis de TV'
-document.writeln(`<h2 class='mt-3'>Modificar un elemento del array - cantidad de elementos ${canciones.length}</h2>`);
-
-document.writeln("<ul class='list-group'>");
-for (let i = 0; i < canciones.length; i++) {
-  document.writeln(`<li class="list-group-item">${canciones[i]}</li>`);
-}
-document.writeln("</ul>");
+canciones[3] = "Sobredosis de TV";
+mostrarCanciones("Modificar un elemento del array");
