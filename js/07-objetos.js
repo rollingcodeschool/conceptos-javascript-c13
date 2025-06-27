44a546f75034becb086146bcd48ac4a5b59165fd
Emilse Arias <emilsearias@gmail.com>
2025-06-26 21:39:01 -0300

    usamos los metodos del objeto pelicula

--- js/07-objetos.js
+++ js/07-objetos.js
@@ -10,13 +10,40 @@ const pelicula ={
     actores: ['Ewan Mcgregor', 'Hayden Christensen'],
     //metodos
     reproducir: function (){
-        document.writeln(`<p>Comenzo la peli ▶ </p>`)
+        console.log(this)
+        document.writeln(`<p>Comenzo la peli ▶ <b>${this.titulo}</b> </p>`)
     },
     detener: () =>{
         document.writeln(`<p>La peli se detuvo ⏹ </p>`)
     }
 }
 
+console.log(this)
 // mostrar el objeto
 console.log(pelicula)
 document.writeln(pelicula)
+document.writeln('<h1>Objeto pelicula</h1>')
+document.writeln(`<p>Titulo: ${pelicula.titulo}</p>`)
+document.writeln(`<p>Duración: ${pelicula.duracion}</p>`)
+document.writeln("<p>Categoria:"+pelicula[`categoria`]+"</p>")
+document.writeln(`<p>Categoria: ${pelicula.categoria}</p>`)
+document.writeln(`<p>Año: ${pelicula.año}</p>`)
+
+//agregar propiedades nuevas
+pelicula.imdb = 7.6
+document.writeln(`<p>IMdb: ${pelicula.imdb}</p>`)
+
+//modificar un objeto
+pelicula.imdb = 8
+document.writeln(`<p>IMdb: ${pelicula.imdb}</p>`)
+
+//borrar propiedad de un objeto
+delete pelicula.puntaje
+console.log(pelicula)
+document.writeln(`<p>Puntaje: ${pelicula.puntaje}</p>`)
+
+//usar los metodos del objeto
+pelicula.reproducir()
+pelicula.detener()
+
+
